645f418738c852d1b8a5c95edc2b6ba6bd6116cb
gvenegas <gvenegas@sionpower.com>
2024-07-31 13:29:37 -0700

    pulse gen rework

--- controllers/pulsegenerator.py
+++ controllers/pulsegenerator.py
@@ -3,18 +3,17 @@ from WF_SDK import device, pattern, error   # import instruments
 
 class PulseGenerator:
     """Diligent Digital Discovery pulse pattern generator"""
-    def __init__(self, device_data):
+    def __init__(self, device_data, pin):
+        self.pin = pin
         self.device_data = device_data
         self.running = False
         
-    def run(self, dio_out, frequency, duty_cycle):
+    def run(self, frequency, duty_cycle):
         """Generate a certain freq PWM signal 
         with certain % duty cycle on a DIO channel.
         Allows user to change pattern on the fly.
         """
-        if self.running:
-            self.stop()
-        pattern.generate(self.device_data, channel=dio_out, 
+        pattern.generate(self.device_data, channel=self.pin, 
                          function=pattern.function.pulse, 
                          frequency=frequency, 
                          duty_cycle=duty_cycle)
@@ -23,10 +22,6 @@ class PulseGenerator:
     def stop(self):
         """stop the pattern generator"""
         if self.running:
-            pattern.close(self.device_data)
+            # hacky, as to not close other pattern channels
+            self.run(1, 0)  
             self.running = False
-
-    def __del__(self):
-        """close the connection"""
-        if self.running:
-            self.stop()
