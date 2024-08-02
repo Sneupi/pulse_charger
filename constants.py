a04b27c29fe6b0d37047d0d6ce3be68830d4c8e8
gvenegas <gvenegas@sionpower.com>
2024-08-02 14:02:05 -0700

    constants & init funcs

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""Constants used to initialize controller 
+instances for the DAQ and SSR devices in 
+the main pulse charge control system"""
+
+# Diligent Digital Discovery
+SSR_CHARGE_PIN = 37
+SSR_DISCHARGE_PIN = 36
+
+# NI USB-6001 DAQ
+SHUNT_PIN1 = 0  # ai0
+SHUNT_PIN2 = 1  # ai1
---
+++ init.py
@@ -0,0 +1,25 @@
+"""Initialization procedures for the pulse charge system"""
+
+from controllers.daq import PulseChargeDAQ       # Voltage & current reader
+from controllers.powersupply import PowerSupply  # PSU Controller
+from controllers.ssr import SSRPulser            # Relay(s) Controller
+
+def psu(psu: PowerSupply):
+    """Power supply init procedure. Asserts
+    system is completely off."""
+    psu.turn_off()
+    assert psu.get_measured_voltage() == 0, \
+        f"Voltage should be 0: {psu.get_measured_voltage()}"
+    assert psu.get_measured_current() == 0, \
+        f"Current should be 0: {psu.get_measured_current()}"
+    
+def ssrs(charge_ssr: SSRPulser, discharge_ssr: SSRPulser):
+    """SSR(s) init procedure. Sets both SSRs to off state."""
+    charge_ssr.shut()
+    discharge_ssr.shut()
+    
+def daq(daq: PulseChargeDAQ):
+    """Shunt reader init procedure"""
+    assert daq.read_battery_voltage() == 0, f"Voltage should be 0: {daq.read_battery_voltage()}"
+    assert daq.read_shunt_current() == 0, f"Current should be 0: {daq.read_shunt_current()}"
+    
