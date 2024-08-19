24b08f1510f6673e6cd3101b57ee51c70f36be19
gvenegas <gvenegas@sionpower.com>
2024-08-19 10:47:36 -0700

    Revert "new const"
    This reverts commit 1b5619651999216aac0732e55901c31d801b26a1.

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ SHUNT_PIN1 = 0  # ai0
 SHUNT_PIN2 = 1  # ai1
 
 # PSU
-PSU_PORT = "COM15"
+PSU_PORT = "COM14"
 
 # SHUNT
 SHUNT_RESISTANCE = 1  # Ohms
@@ -23,17 +23,15 @@ SAMPLE_INTERVAL = 5  # seconds
 
 # BATTERY LIMITS
 BATT_V_HI   = 4.2               # V, battery max op
-BATT_V_LO   = 3.6               # V, battery min op
+BATT_V_LO   = 2.7               # V, battery min op
 
 # RUN PARAMETERS
-STANDING_TIME = 11             # s, idling time after tc
+STANDING_TIME = 2             # s, idling time after tc
 CHG_CURRENT   = 170            # mA, charging amperage
-TC_CUTOFF_I   = 130            # mA, taper charge low cutoff
+TC_CUTOFF_I   = 30            # mA, taper charge low cutoff
 CAP_PCENT_EXIT = .7             # %, exit cycling at this % of initial capacity
 PULSE_FREQ    = 100            # Hz, pulse frequency
 PULSE_DUTY    = 50              # %, pulse duty cycle
 PC_TIMEOUT    = 3600              # s, pulse charge timeout
 TC_TIMEOUT    = 3600              # s, taper charge timeout
-SSR_INPUT_V   = 3.3             # V, SSR input voltage
-SSR_INPUT_I   = 16             # mA, SSR input current
 assert TC_CUTOFF_I > SHUNT_NOISE_THRESH, "Taper charge cutoff current must be above current noise threshold"
